dc6c6e32ba0f9152501dc42ddc5921d5a5994a27
Sadat <sadat0akhtar@gmail.com>
2020-10-04 12:05:32 +0100

    week2 tasks

--- src/App.js
+++ src/App.js
@@ -7,9 +7,13 @@ import Logo from './Logo';
 function App() {
    const abilities = ['Anticipation', 'Adaptability', 'Run-Away'];
    const date = new Date().toLocaleDateString();
+
+   function logWhenClicked(){
+    console.log('wow!!!!');
+}
   return (
     <div className="App">
-      <Logo  appName='Pokedex'/>
+      <Logo  appName='Pokedex'logWhenClicked={}/>
       <BestPokemon abilities={abilities}/>
       <CaughtPokemon d={date} />
       
--- src/Logo.js
+++ src/Logo.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const Logo = (props) => {
+
+    function logWhenClicked(){
+        console.log('wow!!!!');
+    }
     return (
         <header>
             <h1>Welcome to the {props.appName}</h1>
-             <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="logo" />
+             <img onClick={logWhenClicked} src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="logo" />
         </header>
     )
 
